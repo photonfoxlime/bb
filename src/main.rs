726da0a7736933679ea15368656b1769f4edfa35
LitiaEeloo <litiaeeloo@gmail.com>
2026-02-24 11:30:14 -0500

    repo: update project name

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ mod undo;
 const DEFAULT_FONT: iced::Font = iced::Font::with_name("LXGW WenKai");
 
 fn main() -> iced::Result {
+    #[cfg(feature = "log")]
     init_tracing();
     iced::application(app::AppState::load, app::update, app::view)
         .subscription(app::subscription)
@@ -29,10 +30,11 @@ fn main() -> iced::Result {
         .font(lucide_icons::LUCIDE_FONT_BYTES)
         .default_font(DEFAULT_FONT)
         .theme(|state: &app::AppState| theme::app_theme(state.is_dark))
-        .title("Block Bunny")
+        .title("Blooming Blockery")
         .run()
 }
 
+#[cfg(feature = "log")]
 fn init_tracing() {
     let env_filter = tracing_subscriber::EnvFilter::try_from_default_env()
         .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new("bb=info"));
